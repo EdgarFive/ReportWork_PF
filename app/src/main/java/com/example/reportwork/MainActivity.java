e43b85f64b37d805cdfd25b8164f312cc5b0d7bd
Edgar <5910usuario@gmail.com>
2024-10-23 00:43:19 -0600

    Ya vamos acanzando de apoco. Cree las clases centrales y los activitys, aunque no he metido codigo.

--- app/src/main/java/com/example/reportwork/MainActivity.java
+++ app/src/main/java/com/example/reportwork/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.reportwork;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +12,32 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn_crear_reporte, btn_ver_reporte;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btn_crear_reporte = findViewById(R.id.btn_crear_reporte);
+        btn_ver_reporte = findViewById(R.id.btn_ver_reporte);
+
+
+        //Boton 1: Crear reporte nuevo =======================================================
+        btn_crear_reporte.setOnClickListener(v ->{
+            Intent intent = new Intent(MainActivity.this, ReporteActivity.class);
+            startActivity(intent);
+        });
+
+        //Boton 2: Ver reportes ======================================================
+        btn_ver_reporte.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, VerReporteActivity.class);
+            startActivity(intent);
+        });
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
---
+++ app/src/main/java/com/example/reportwork/VerReporteActivity.java
@@ -0,0 +1,4 @@
+package com.example.reportwork;
+
+public class VerReporteActivity {
+}
